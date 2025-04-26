0c8cf0ee6f0d40677846e7b19fa36e2a6a90eecd
Carlos Yerena <carlos@mangos77.mx>
2025-04-26 00:39:06 +0000

    feat: all de local ip address are allow to call endpoints

--- src/config.js
+++ src/config.js
@@ -1,9 +1,28 @@
 const path = require('path')
+const os = require('os')
 
 // Por si se desea tomar datos del package.json
 const pkjson = require(path.resolve(__basedir, '..', 'package.json'))
 
 const config = () => {
+    function getLocalIPs() {
+        const interfaces = os.networkInterfaces();
+        const ips = [];
+
+        for (const name of Object.keys(interfaces)) {
+            if (name !== 'eth0' && name !== 'wlan0') continue; // solo eth0 y wlan0 como pediste
+
+            for (const net of interfaces[name] || []) {
+                if (net.family === 'IPv4' && !net.internal) {
+                    ips.push(net.address);
+                }
+            }
+        }
+
+        return ips;
+    }
+    const localIPs = getLocalIPs()
+
     const config_dev = {
         name: pkjson.name,
         version: pkjson.version,
@@ -16,7 +35,7 @@ const config = () => {
     const config_prod = {
         production: true,
         port: process.env.PORT || 8080,
-        allowHosts: ['localhost', '127.0.0.1', 'iface=lo', 'm77panel.local'],
+        allowHosts: ['localhost', '127.0.0.1', 'iface=lo', 'm77panel.local'].concat(localIPs),
         wifi_config: { debugLevel: 0 }
     }
 
@@ -25,5 +44,5 @@ const config = () => {
         return Object.assign({}, config_dev, config_prod)
     else
         return config_dev
-} 
+}
 module.exports = config()
