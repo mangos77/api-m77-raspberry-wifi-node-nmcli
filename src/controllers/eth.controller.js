88429025a8d840fb87805051216205914b129856
Carlos Yerena <carlos@mangos77.mx>
2025-09-10 14:37:21 +0000

    fix: bussy state

--- src/controllers/eth.controller.js
+++ src/controllers/eth.controller.js
@@ -7,6 +7,7 @@ import { getSocketIO } from '../modules/socketio.module.js'
 
 class Controller {
     #eth = null
+    #bussy = false
     #options = {}
     constructor() {
         this.#eth = new M77RaspberryETH()
@@ -14,39 +15,49 @@ class Controller {
 
     socketioCalls = () => {
         setInterval(async () => {
-            const io = getSocketIO();
+            if (!this.#bussy) {
+                const io = getSocketIO();
 
-            // Info Ethernet
-            const eths = await this.#eth.status()
-            if (eths.success) {
-                try {
-                    io.emit('eth_status', eths.data);
-                } catch (e) { }
+                // Info Ethernet
+                const eths = await this.#eth.status()
+                if (eths.success) {
+                    try {
+                        io.emit('eth_status', eths.data);
+                    } catch (e) { }
+                }
             }
         }, 1000)
     }
 
     init = async (options = {}) => {
         return new Promise(async (resolve, reject) => {
+            this.#bussy = true
             const result = await this.#eth.init(options)
             this.socketioCalls()
+            this.#bussy = false
             resolve(result)
         })
     }
 
     listInterfaces = async (req, res) => {
+        this.#bussy = true
         const result = await this.#eth.listInterfaces()
+        this.#bussy = false
         res.json(result)
     }
 
     status = async (req, res) => {
+        this.#bussy = true
         const result = await this.#eth.status()
+        this.#bussy = false
         res.json(result)
     }
 
     setConnection = async (req, res) => {
+        this.#bussy = true
         const config_net = { ...{}, ...req.body }
         const result = await this.#eth.setConnection(config_net)
+        this.#bussy = false
         res.json(result)
     }
 
--- src/controllers/wifi.controller.js
+++ src/controllers/wifi.controller.js
@@ -2,11 +2,12 @@
     Wifi controller
 */
 
-import {M77RaspberryWIFI} from 'm77-raspberry-wifi-node-nmcli'
+import { M77RaspberryWIFI } from 'm77-raspberry-wifi-node-nmcli'
 import { getSocketIO } from '../modules/socketio.module.js'
 
 class Controller {
     #wifi = null
+    #bussy = false
     #options = {}
     constructor() {
         this.#wifi = new M77RaspberryWIFI()
@@ -14,100 +15,126 @@ class Controller {
 
     socketioCalls = () => {
         setInterval(async () => {
-            const io = getSocketIO();
+            if (!this.#bussy) {
+                const io = getSocketIO();
 
-            // Info Wifi
-            const wifistatus = await this.#wifi.status(true)
-            if (wifistatus.success) {
-                try {
-                    io.emit('wifi_status', wifistatus.data);
-                } catch (e) { }
-            }
+                // Info Wifi
+                const wifistatus = await this.#wifi.status(true)
+                if (wifistatus.success) {
+                    try {
+                        io.emit('wifi_status', wifistatus.data);
+                    } catch (e) { }
+                }
 
-            // Saved Wifi
-            const wifisaved = await this.#wifi.savedNetworks()
-            if (wifisaved.success) {
-                try {
-                    io.emit('wifi_saved', wifisaved.data);
-                } catch (e) { }
+                // Saved Wifi
+                const wifisaved = await this.#wifi.savedNetworks()
+                if (wifisaved.success) {
+                    try {
+                        io.emit('wifi_saved', wifisaved.data);
+                    } catch (e) { }
+                }
             }
         }, 1000)
 
         setInterval(async () => {
-            const io = getSocketIO();
+            if (!this.#bussy) {
+                const io = getSocketIO();
 
-            // Scan Wifi
-            const wifiscan = await this.#wifi.scan()
-            if (wifiscan.success) {
-                try {
-                    io.emit('wifi_scan', wifiscan.data);
-                } catch (e) { }
+                // Scan Wifi
+                const wifiscan = await this.#wifi.scan()
+                if (wifiscan.success) {
+                    try {
+                        io.emit('wifi_scan', wifiscan.data);
+                    } catch (e) { }
+                }
             }
         }, 5000)
     }
 
     init = async (options = {}) => {
         return new Promise(async (resolve, reject) => {
+            this.#bussy = true
             const result = await this.#wifi.init(options)
             this.socketioCalls()
+            this.#bussy = false
             resolve(result)
         })
     }
 
     listInterfaces = async (req, res) => {
+        this.#bussy = true
         const result = await this.#wifi.listInterfaces()
+        this.#bussy = false
         res.json(result)
     }
 
     status = async (req, res) => {
+        this.#bussy = true
         const withConnectionInfo = req.query.withConnectionInfo === "true" ? true : false
         const result = await this.#wifi.status(withConnectionInfo)
+        this.#bussy = false
         res.json(result)
     }
 
 
     savedNetworks = async (req, res) => {
+        this.#bussy = true
         const exclude_words_arr = req.query.exclude ? req.query.exclude.split(',').map(word => word.trim()) : []
         const result = await this.#wifi.savedNetworks(exclude_words_arr)
+        this.#bussy = false
         res.json(result)
     }
 
     removeNetwork = async (req, res) => {
+        this.#bussy = true
         const ssid = req.query.ssid ? req.query.ssid.trim() : ''
         const result = await this.#wifi.removeNetwork(ssid)
+        this.#bussy = false
         res.json(result)
     }
 
     removeAllNetworks = async (req, res) => {
+        this.#bussy = true
         const result = await this.#wifi.removeAllNetworks()
+        this.#bussy = false
         res.json(result)
     }
 
     scan = async (req, res) => {
+        this.#bussy = true
         const result = await this.#wifi.scan()
+        this.#bussy = false
         res.json(result)
     }
 
     connect = async (req, res) => {
+        this.#bussy = true
         const config_net = { ...{}, ...req.body }
         const result = await this.#wifi.connect(config_net)
+        this.#bussy = false
         res.json(result)
     }
 
     reconnect = async (req, res) => {
+        this.#bussy = true
         const config_net = { ...{}, ...req.body }
         const result = await this.#wifi.reconnect(config_net)
+        this.#bussy = false
         res.json(result)
     }
 
     disconnect = async (req, res) => {
+        this.#bussy = true
         const result = await this.#wifi.disconnect()
+        this.#bussy = false
         res.json(result)
     }
 
     setConnection = async (req, res) => {
+        this.#bussy = true
         const config_net = { ...{}, ...req.body }
         const result = await this.#wifi.setConnection(config_net)
+        this.#bussy = false
         res.json(result)
     }
 }
